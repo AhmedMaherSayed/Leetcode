5f93aa2e0c61201d7fe3adccd61d038bfd993e0b
Ahmedmaher2 <62140280+Ahmedmaher2@users.noreply.github.com>
2022-11-26 00:18:01 +0200

    Time: 31 ms (46.76%), Space: 5.9 MB (32.80%) - LeetHub

---
+++ 0009-palindrome-number/0009-palindrome-number.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    bool isPalindrome(int x) {
+        long long temp = x, reversed = 0;
+        if(x < 0)
+        {
+            return false;
+        }
+        else if (x > 0 && x < 10)
+        {
+            return true;
+        }
+        else
+        {
+            while(temp > 0)
+            {
+                reversed = reversed * 10 + temp % 10;
+                temp /= 10;
+            }
+            if (reversed == x)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        
+        
+    }
+};
