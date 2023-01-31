52338ecdd12a3d67174a066ec269885bdd388d8f
Ahmed Maher <62140280+AhmedMaherSayed@users.noreply.github.com>
2023-01-31 16:35:51 +0200

    Time: 192 ms (24.94%), Space: 51.6 MB (35.92%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.js
+++ 0217-contains-duplicate/0217-contains-duplicate.js
@@ -3,5 +3,14 @@
  * @return {boolean}
  */
 var containsDuplicate = function(nums) {
-    const s = new Set(nums); return s.size !== nums.length
-};
+    let counter = 0;
+   nums.sort();
+    for(let i = 1; i < nums.length; i++)
+        {
+            if(nums[i] === nums[i -1])
+                {
+                    return true;
+                }
+        }
+    return false;
+}
