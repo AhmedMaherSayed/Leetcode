3ab3d287e502a3a5cf652425771762e9906d323d
Ahmed Maher <62140280+AhmedMaherSayed@users.noreply.github.com>
2023-02-19 07:15:58 +0200

    Time: 75 ms (21.56%), Space: 42.2 MB (24.27%) - LeetHub

---
+++ 1678-goal-parser-interpretation/1678-goal-parser-interpretation.js
@@ -0,0 +1,10 @@
+/**
+ * @param {string} command
+ * @return {string}
+ */
+var interpret = function(command) {
+    
+    command = command.replaceAll('()' , 'o');
+    command = command.replaceAll("(al)" , "al");
+    return command;
+};
