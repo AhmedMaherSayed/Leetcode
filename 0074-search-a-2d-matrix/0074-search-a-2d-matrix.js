d95f2821f0a5e43b7645e6d3588b6013e085f267
Ahmed Maher <62140280+AhmedMaherSayed@users.noreply.github.com>
2023-08-07 08:34:49 +0300

    Time: 48 ms (92.89%), Space: 41.9 MB (74.48%) - LeetHub

---
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.js
@@ -0,0 +1,19 @@
+/**
+ * @param {number[][]} matrix
+ * @param {number} target
+ * @return {boolean}
+ */
+var searchMatrix = function(matrix, target) {
+    let flag = false;
+    for(let i = 0; i < matrix.length; i++)
+        {
+            for(let j= 0; j < matrix[i].length; j++)
+                {
+                    if(matrix[i][j] === target)
+                        {
+                            flag = true;
+                        }
+                }
+        }
+    return flag;
+};
