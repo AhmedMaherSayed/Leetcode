bb726274e70e4e4b961d099a3c6d83b6e272690d
Ahmed Maher <62140280+AhmedMaherSayed@users.noreply.github.com>
2023-09-28 08:02:51 +0300

    Time: 73 ms (23.84%), Space: 43.6 MB (99.02%) - LeetHub

--- 0905-sort-array-by-parity/0905-sort-array-by-parity.js
+++ 0905-sort-array-by-parity/0905-sort-array-by-parity.js
@@ -4,16 +4,17 @@
  */
 var sortArrayByParity = function(nums) {
     let ans = [];
+    
     for(let i = 0; i < nums.length; i++)
         {
-            if(nums[i] % 2 === 0)
+            if(nums[i] % 2 == 0)
                 {
                     ans.push(nums[i]);
                 }
         }
     for(let i = 0; i < nums.length; i++)
         {
-            if(nums[i] % 2 !== 0)
+            if(nums[i] % 2 != 0)
                 {
                     ans.push(nums[i]);
                 }
