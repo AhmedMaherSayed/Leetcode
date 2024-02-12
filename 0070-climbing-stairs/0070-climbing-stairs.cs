5d465d029314b3a90c13a0251c2b578cc5060dd2
Ahmed Maher <62140280+AhmedMaherSayed@users.noreply.github.com>
2024-02-12 08:12:46 +0200

    Time: 18 ms (79.09%), Space: 26.9 MB (14.12%) - LeetHub

---
+++ 0070-climbing-stairs/0070-climbing-stairs.cs
@@ -0,0 +1,17 @@
+public class Solution {
+    public int ClimbStairs(int n) {
+      int fibPrev = 1, fibNow = 1, fibNext;
+
+        if (n == 1)
+        {
+            return 1;
+        }
+        for(int i = 2; i <= n; i++)
+        {
+            fibNext = fibPrev + fibNow;
+            fibPrev = fibNow;
+            fibNow = fibNext;
+        }
+        return fibNow;  
+    }
+}
