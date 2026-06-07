6f7b9e9f99d6fa69b967b4ca08c3ca3cd05ed4cd
Ahmed Maher <62140280+AhmedMaherSayed@users.noreply.github.com>
2026-06-07 06:40:07 +0300

    Time: 31 ms (19.11%), Space: 44.3 MB (63.12%) - LeetHub

---
+++ 0242-valid-anagram/0242-valid-anagram.cs
@@ -0,0 +1,19 @@
+public class Solution {
+    public bool IsAnagram(string s, string t) {
+        var sArr = s.ToCharArray();
+        var tArr = t.ToCharArray();
+
+        Array.Sort(sArr);
+        Array.Sort(tArr);
+
+        if (sArr.Length != tArr.Length)
+            return false;
+
+        for (int i = 0; i < sArr.Length; i++)
+        {
+            if (sArr[i] != tArr[i])
+                return false;
+        }
+        return true;
+    }
+}
