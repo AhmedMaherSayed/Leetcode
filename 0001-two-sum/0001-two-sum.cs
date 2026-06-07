a0ca56c74228e943fe8a1d06a81390381e40e26b
Ahmed Maher <62140280+AhmedMaherSayed@users.noreply.github.com>
2026-06-07 05:53:09 +0300

    Time: 21 ms (42.91%), Space: 50.9 MB (5.57%) - LeetHub

--- 0001-two-sum/0001-two-sum.cs
+++ 0001-two-sum/0001-two-sum.cs
@@ -1,14 +1,24 @@
 public class Solution {
     public int[] TwoSum(int[] nums, int target) {
-     
-     for (int i =0; i < nums.Length; i++)
+        var indexed = nums.Select((val, idx) => (val, idx))
+                          .OrderBy(x => x.val)
+                          .ToArray();
+
+     int leftPtr = 0;
+     int rightPtr = nums.Length - 1;
+    int sum = 0;
+     while (leftPtr < rightPtr)
      {
-        for (int j = i + 1; j < nums.Length; j++)
-        {
-            if (nums[i] + nums[j] == target)
-                return [i, j];
-        }
+        sum = indexed[leftPtr].val + indexed[rightPtr].val;
+
+        if (sum == target)
+            return [indexed[leftPtr].idx, indexed[rightPtr].idx];
+        else if (sum > target)
+            rightPtr--;
+        else if (sum < target)
+            leftPtr++;
      }
-    return [];
+
+     return [];
     }
 }
