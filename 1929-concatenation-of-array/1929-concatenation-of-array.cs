dcb0bdfe19bf4244f490e29f58b691fab8f4d4ca
Ahmed Maher <62140280+AhmedMaherSayed@users.noreply.github.com>
2026-06-16 13:48:16 +0300

    Time: 0 ms (100%), Space: 52.1 MB (33.07%) - LeetHub

--- 1929-concatenation-of-array/1929-concatenation-of-array.cs
+++ 1929-concatenation-of-array/1929-concatenation-of-array.cs
@@ -1,12 +1,13 @@
 public class Solution {
     public int[] GetConcatenation(int[] nums) {
-        int[] ans = new int[nums.Length * 2];
-        
-        for(int i = 0; i < nums.Length; i++)
-        {
-            ans[i] = nums[i];
-            ans[i + nums.Length] = nums[i];
-        }
-        return ans;
+            int capacity = nums.Length;
+            int[] ans = new int[capacity * 2];
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                ans[i] = nums[i];
+                ans[i + capacity] = nums[i];
+            }
+            return ans; 
     }
 }
